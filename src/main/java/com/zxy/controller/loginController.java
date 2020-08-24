a640f60416753c83d2000e3d204d0b89c658b392
zllzxy123 <654719289@qq.com>
2020-08-24 18:10:39 +0800

    第一次

---
+++ src/main/java/com/zxy/controller/loginController.java
@@ -0,0 +1,14 @@
+package com.zxy.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/login")
+public class loginController {
+    @GetMapping("/do/{username}")
+    public String dologin(@PathVariable("username")String username, Model model){
+        model.addAttribute("name",username);
+        return "index";
+    }
+}
---
+++ src/test/java/com/zxy/NoproblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoproblemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
